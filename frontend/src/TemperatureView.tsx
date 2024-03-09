23f3774e89c13fc6732436de6d0a18b0b55cfcea
hlvlad <hl.vladyslav@gmail.com>
2024-03-09 17:32:50 +0100

    Configure Cargo Workspace, tweak UI button to not flicker on reload

--- frontend/src/TemperatureView.tsx
+++ frontend/src/TemperatureView.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button, Center, Flex, Text } from '@chakra-ui/react';
 import ky from 'ky';
 
@@ -7,22 +7,32 @@ interface TemperatureResponse {
 }
 
 const TemperatureView = () => {
-    const [temperature, setTemperature] = useState<number | null>(null);
+    const [temperature, setTemperature] = useState<number | undefined>();
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        fetchTemperature();
+    }, []);
 
     const fetchTemperature = async () => {
+        const timer = setTimeout(() => setIsLoading(true), 100);
         try {
             const data = await ky.get('/api/temperature').json<TemperatureResponse>();
+            clearTimeout(timer);
             setTemperature(data.temperature);
         } catch (error) {
+            clearTimeout(timer);
             console.error('Error fetching temperature:', error);
         }
+
+        setIsLoading(false);
     };
 
     return (
         <Flex justifyContent="center" alignItems="center" height="100vh">
             <Center flexDirection="column">
                 <Text fontSize="8xl" fontWeight="bold" mb={4}>
-                    {temperature !== null ? `${temperature}°C` : 'Loading...'}
+                    {isLoading ? 'Loading...' : temperature ? `${temperature}°C` : '\u00A0' /* Non-breaking space */}
                 </Text>
                 <Button colorScheme="teal" size="lg" onClick={fetchTemperature}>
                     Update Temperature
