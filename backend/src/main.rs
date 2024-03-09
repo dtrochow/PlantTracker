51b369d7b3e95f984fa2b76da112221b5e2f0c51
hlvlad <hl.vladyslav@gmail.com>
2024-03-09 17:32:50 +0100

    Configure Cargo Workspace, tweak UI button to not flicker on reload

---
+++ backend/src/main.rs
@@ -0,0 +1,35 @@
+#[cfg(not(debug_assertions))]
+use actix_files::Files;
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use rand::Rng;
+use serde_json::json;
+
+#[get("/api/temperature")]
+async fn temperature() -> impl Responder {
+    let temperature_cels = rand::thread_rng().gen_range(0..100);
+    HttpResponse::Ok().json(json!({
+        "temperature": temperature_cels
+    }))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenvy::from_filename(".env.dev").expect("Failed to load .env.dev file");
+
+    HttpServer::new(move || {
+        let app = App::new()
+            .service(temperature);
+
+        // Serve static files only in production mode
+        #[cfg(not(debug_assertions))]
+        app.service(Files::new(
+            "/", 
+            std::env::var("FRONTEND_STATIC_PATH").expect("FRONTEND_STATIC_PATH environment variable wasn't found. Specify location of bundled frontend files using FRONTEND_STATIC_PATH environment variable.")
+        ).index_file("index.html"));
+
+        app
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
